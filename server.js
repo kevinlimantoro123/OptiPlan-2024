48cba18df05c2441719785bcccdd295f44c05410
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2024-05-18 01:02:36 +0700

    new user auth

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const app = express();
 
 const port = process.env.PORT || 3000; //looks for any free port. If not, use 3000
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   //Initialise request and response
-  res.status(200).send("START");
+  res.render("index");
+});
+
+app.get("/users/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/users/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/users/dashboard", (req, res) => {
+  res.render("dashboard", { user: "TEST USER" });
 });
 
 app.listen(port, () => {
