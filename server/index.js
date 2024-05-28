fbb4dc1038f7f12e4086aceb58c6f8f44c11e9d1
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2024-05-28 17:56:51 +0700

    new

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwtGenerator = require("./utils/jwtGenerator");
 
 app.use(cors());
 app.use(express.json());
@@ -10,9 +9,6 @@ app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard", require("./routes/dashboard"));
 app.use("/admin", require("./routes/admin"));
 app.use("/calendar", require("./routes/calendar"));
-app.use("/auth", require("./routes/jwtAuth"));
-app.use("/dashboard", require("./routes/dashboard"));
-app.use("/admin", require("./routes/admin"));
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
