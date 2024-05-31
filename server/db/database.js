ff55bb298cd0e9abd93686c94578a1e29caa8c37
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2024-05-31 18:01:21 +0700

    new

--- server/db/database.js
+++ server/db/database.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "optiplan",
+  password: "YpubqnvztNKmrgGUhQJCEijalIzpvBle",
+  host: "viaduct.proxy.rlwy.net",
+  port: 46433,
+  database: "railway",
 });
 
 module.exports = pool;
