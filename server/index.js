a32548d9f0f0f5f3e17da0d0d0636413b641f28f
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2024-05-31 17:43:36 +0700

    testing

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use("/auth", require("./routes/jwtAuth"));
 app.use("/dashboard", require("./routes/dashboard"));
 app.use("/admin", require("./routes/admin"));
 app.use("/calendar", require("./routes/calendar"));
+app.use("/test", require("./routes/test"));
 
 app.listen(5000, () => {
   console.log("Server started on port 5000");
