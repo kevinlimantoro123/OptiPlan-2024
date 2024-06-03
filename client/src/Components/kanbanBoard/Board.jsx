4df2a6428100b46fe2d4e27b764700565f1fb3aa
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2024-06-03 16:45:01 +0700

    integrated column.jsx into board.jsx

--- client/src/Components/kanbanBoard/Board.jsx
+++ client/src/Components/kanbanBoard/Board.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
-import Column from "./Column";
 
 const Board = () => {
-  const [cards, setCards] = useState("");
+  const [cards, setCards] = useState([]);
 
   async function getEvents() {
     try {
@@ -25,39 +24,60 @@ const Board = () => {
     <div className="flex h-full w-full gap-3 overflow-scroll p-12">
       <Column
         title="Meeting"
-        column="meeting"
-        headingColor="text-neutral-500"
+        label="indigo"
+        headingColor="text-indigo-500"
         cards={cards}
         setCards={setCards}
       />
       <Column
         title="Work"
-        column="work"
-        headingColor="text-yellow-200"
+        label="red"
+        headingColor="text-red-200"
         cards={cards}
         setCards={setCards}
       />
       <Column
         title="Class"
-        column="class"
+        label="blue"
         headingColor="text-blue-200"
         cards={cards}
         setCards={setCards}
       />
       <Column
         title="Important"
-        column="important"
-        headingColor="text-emerald-200"
+        label="green"
+        headingColor="text-green-200"
         cards={cards}
         setCards={setCards}
       />
       <Column
         title="Personal"
-        column="personal"
-        headingColor="text-black-200"
+        label="purple"
+        headingColor="text-purple-200"
         cards={cards}
         setCards={setCards}
       />
+    </div>
+  );
+};
+
+const Column = ({ title, headingColor, cards, label, setCards }) => {
+  const [active, setActive] = useState(false);
+
+  const filteredCards = cards.filter((e) => e.label === label);
+  return (
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={`font-medium ${  }`}>{title}</h3>
+        <span className="rounded text-sm text-neutral-400">
+          {filteredCards.length}
+        </span>
+      </div>
+      <div
+        className={`h-full w-full transition-colors ${
+          !active ? "bg-neutral-800/50" : "bg-neutral-800/0"
+        }`}
+      ></div>
     </div>
   );
 };
