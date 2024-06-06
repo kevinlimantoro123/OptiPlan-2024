809818016dd1b36f2ebb7cf5e52ca5190db45d05
kennylewi <kennylewi27@gmail.com>
2024-06-06 20:19:38 +0700

    working week scheduler

--- client/src/Components/calendar/HourSlot.js
+++ client/src/Components/calendar/HourSlot.js
@@ -3,7 +3,7 @@ export default function HourSlot({ hour }) {
         <div className="-z-30 border border-gray-200 flex flex-col">
             <header className="flex flex-col items-end">
                 <div className="h-10">
-                    <p className={`text-m p-1 pr-4 my-1 text-center`}>
+                    <p className="text-m p-1 pr-4 my-1 text-center">
                         {hour}
                     </p>
                 </div>
