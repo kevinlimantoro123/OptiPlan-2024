e6e6ec458236e3d0bd70c89342b3d2ae5f17faed
kevinlimantoro123 <kevinlimantoro123@gmail.com>
2024-07-23 18:35:40 +0700

    NEW

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/jwtAuth"));
@@ -10,6 +12,6 @@ app.use("/calendar", require("./routes/calendar"));
 app.use("/kanban", require("./routes/kanban"));
 app.use("/notification", require("./routes/notification"));
 
-app.listen(5000, () => {
+app.listen(process.env.PGPORT, () => {
   console.log("Server started on port 5000");
 });
