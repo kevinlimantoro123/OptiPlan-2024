3c0991ce2f548ce6ce749052488febb3ce9c9f33
KennyLewi <kennylewi27@gmail.com>
2024-07-27 12:21:25 +0700

    removed "console.table" from Month.js

--- client/src/Components/calendar/Month.js
+++ client/src/Components/calendar/Month.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Day from "./Day";
 export default function Month({ month }) {
-  console.table(month);
   return (
     <div className="flex-1 grid grid-cols-7 grid-rows-5 bg-neutral-900">
       {month.map((row, i) => (
